2e33b80593c51960223c23edf92c68f3107e9e82
aslac2020 <andredesenvolvedorti@gmail.com>
2021-09-09 22:47:00 -0300

    apiJogo

---
+++ Cria tabela dbo.Jogo.sql
@@ -0,0 +1,19 @@
+﻿USE [CatalogoJogos]
+GO
+
+/****** Object: Table [dbo].[Jogo] Script Date: 22/06/2021 14:03:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Jogos] (
+    [Id]        UNIQUEIDENTIFIER   NOT NULL PRIMARY KEY,
+    [Nome]      NCHAR (100)    NULL,
+    [Produtora] NCHAR (100)    NULL,
+    [Preco]     FLOAT	    NULL 
+);
+
+
+Drop table jogos;
